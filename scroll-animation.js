3706962ba62b131950fa8258cb021c74f19029fb
Ismail Ahmad <ismailahmad0505@gmail.com>
2025-04-25 03:51:24 +0500

    added our client section

---
+++ scroll-animation.js
@@ -0,0 +1,19 @@
+let list = document.querySelector(".logos-container");
+const speed = 2;
+
+
+function scrollAnimation() {
+    list.scrollLeft += speed;
+    const firstItem = list.children[0];
+    const gap = parseInt(getComputedStyle(list).gap);
+    const marginRight = parseInt(getComputedStyle(firstItem).marginRight);
+    const fullWidth = firstItem.offsetWidth + gap + marginRight;
+    if(list.scrollLeft >= fullWidth) {
+        list.appendChild(firstItem);
+        list.scrollLeft -= fullWidth;
+    }
+    requestAnimationFrame(scrollAnimation);
+    
+}
+
+scrollAnimation();
